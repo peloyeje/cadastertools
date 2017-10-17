f83fae17b3ed1b00b10b146f727b545bc47c42c5
aredier <antoine.redier2@gmail.com>
2017-10-17 18:26:02 +0200

    get functional version of get_areas

--- R/get_areas.R
+++ R/get_areas.R
@@ -1,14 +1,16 @@
 #' get_areas
 #'
-#' @param query_adress
-#' @param data
+#' @param polygon_df your SpatialPolygonsDataFrame
 #'
 #' @return returns the area of the polygon of a given address
 #' @export
 #' @importFrom methods slot
 #' @examples
+#' \dontrun{
+#' get_areas(polygon_df)
+#' }
 get_areas <- function(polygon_df){
   polygon_list <- lapply(polygon_df@polygons , methods::slot , "Polygons")
-  areas_list <- lapply(polygon_list[[1]], function(x) slot(x, "area"))
+  areas_list <- lapply(polygon_list, function(x) slot(x[[1]], "area"))
   return(areas_list)
 }
