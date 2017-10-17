7d41b751cd4bdd079c7fc96b93babbd4f817fde2
aredier <antoine.redier2@gmail.com>
2017-10-17 09:18:23 +0200

    supressed message banR::geocode's staus mesage

--- R/get_adress_data.R
+++ R/get_adress_data.R
@@ -11,6 +11,8 @@
 #' get_adress_data("12, rue de Lota 75116 Paris")
 #' }
 get_adress_data <- function(query_adress){
+  suppressMessages(
 adress_data <- banR::geocode(query = query_adress)
+)
 return(adress_data)
 }
