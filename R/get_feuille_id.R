b7decfe70801a4065e69b3019f47b543d0c67d5a
Régis Bouyoux <regis.bouyoux@hec.edu>
2017-10-18 15:51:59 +0200

    Commit get_feuille_id for the first time add to get the feuille number for the polygon of an address, previously output by select_polygone

---
+++ R/get_feuille_id.R
@@ -0,0 +1,15 @@
+#' get_feuille_id
+#' Fonction qui donne la liste des numéros de feuilles pour un SpatialPolygonDataframe construit avec le cadastre
+#'
+#' @param polygon : SpatialPolygoneDataFrame, output de get_cadaster_sp (cadaster_type = "feuilles")
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_feuille_id <- function(polygon){
+  numero_feuille <- polygon[[2]]@data["id"]
+  return(numero_feuille)
+}
+
+# faire : select_polygone(adresse_antoine) selectionner la feuille
