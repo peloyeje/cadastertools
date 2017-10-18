ee2d02df1dd7c3f27c649d46ed3e588d70b88061
aredier <antoine.redier2@gmail.com>
2017-10-18 11:15:26 +0200

    get_areas return in meters and in a vector (instead of a list a previously)

--- R/get_areas.R
+++ R/get_areas.R
@@ -4,13 +4,18 @@
 #'
 #' @return returns the area of the polygon of a given address
 #' @export
+#' @import sp
 #' @importFrom methods slot
+#'
 #' @examples
 #' \dontrun{
 #' get_areas(polygon_df)
 #' }
 get_areas <- function(polygon_df){
+  polygon_df <- sp::spTransform(polygon_df, sp::CRS(paste("+proj=merc +lat_ts=48.84 +units=m")))
   polygon_list <- lapply(polygon_df@polygons , methods::slot , "Polygons")
-  areas_list <- lapply(polygon_list, function(x) slot(x[[1]], "area"))
-  return(areas_list)
+  areas_list <- lapply(polygon_list, function(x){
+    return(slot(x[[1]], "area"))
+  })
+  return(unlist(areas_list))
 }
