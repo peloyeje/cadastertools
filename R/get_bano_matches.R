67286bd180eec271a8a0fab4dd9dd9fd0efd13c2
Jean-Eudes Peloye <hello@peloye.je>
2017-11-07 11:35:25 +0100

    Rename get_adress_data to get_bano_matches

---
+++ R/get_bano_matches.R
@@ -0,0 +1,25 @@
+#' Get BANO matches
+#'
+#' Retrieves all addresses (+ useful info) that match the address query in the BANO database
+#'
+#' @param address String to query
+#'
+#' @return A dataframe containing BANO search results
+#' @export
+#' @import banR
+#' @import assertthat
+#'
+#' @examples
+#' \dontrun{
+#' get_bano_matches("12, rue de Lota 75116 Paris")
+#' }
+get_bano_matches <- function(address) {
+  assertthat::assert_that(
+    length(address) != 0,
+    msg = "You must enter an address"
+  )
+
+  return(
+    banR::geocode(query = address)
+  )
+}
